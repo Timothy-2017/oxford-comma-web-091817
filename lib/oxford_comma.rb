e103a781bd7e015e5a19555f74fc25a993f3c7ae
Tim Eubank <timothy.eubank@gmail.com>
2017-09-06 22:26:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join('')
+  elsif array.size == 2
+    array.join(' and ')
+  else
+    array_main = array
+    array_last = ', and '
+    array_last = array_last.concat(array.pop)
+    sentence = array_main.join(", ")
+    sentence = sentence.concat(array_last)
+  end
 end
